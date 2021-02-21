6a233ddb425df49c95ff4a9ec9bcceb06cb1626d
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-02-21 15:58:15 +0300

    add arrays of the male and female names

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,9 @@ window.addEventListener("DOMContentLoaded", function (){
   dateForm.addEventListener("submit", function(event) {
     event.preventDefault();
     console.log("Form submitted");
+
+    let maleNames = ["Kwasi" "Kwadwo" "Kwabena" "Kwaku" "Yaw" "Kofi" "Kwame"]
+    let femaleNames = ["Akosua" "Adwoa" "Abenaa" "Akua" "Yua" "Yaa" "Afua" "Ama"]
   }
   )
 });
