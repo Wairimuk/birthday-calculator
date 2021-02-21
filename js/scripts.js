b83391ca2b1bb45732b1637c5ec234e49dd19e95
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-02-21 19:58:32 +0300

    add a function to get a day of the week from a given date

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,23 @@ window.addEventListener("DOMContentLoaded", function (){
     event.preventDefault();
     console.log("Form submitted");
 
-    let maleNames = ["Kwasi" "Kwadwo" "Kwabena" "Kwaku" "Yaw" "Kofi" "Kwame"]
-    let femaleNames = ["Akosua" "Adwoa" "Abenaa" "Akua" "Yua" "Yaa" "Afua" "Ama"]
+  //   let dayOfWeek = (year*365 + trunc((year-1) / 4) - trunc((year-1) / 100) +
+  //   trunc((year-1) / 400)) % 7
+
+  //   let femaleNames = ["Akosua" "Adwoa" "Abenaa" "Akua" "Yua" "Afua" "Ama"];
+  //   let maleNames = ["Kwasi" "Kwadwo" "Kwabena" "Kwaku" "Yaw" "Kofi" "Kwame"];
+    
+  //   let akanName = function
+
+  // }
+  // )
+  function getDayName(dateStr, locale)
+  {
+      var date = new Date(dateStr);
+      return date.toLocaleDateString(locale, { weekday: 'long' });        
   }
-  )
+  
+  var dateStr = '05/23/2014';
+  var day = getDayName(dateStr, "en-US");
+  
 });
