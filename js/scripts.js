ddc2366428ec69c097877e906e5c641ac4beb2e6
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-02-21 11:39:17 +0300

    add an eventlistener to the submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 window.addEventListener("DOMContentLoaded", function (){
   console.log("Content Loaded");
 
+  let submitBtn = document.querySelector("#btn");
+  let dateForm = document.querySelector("#date-form");
+
+  dateForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("Form submitted");
+  }
+  )
 });
