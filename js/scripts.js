6a8afe0f9040df0b63a61261294685e133565400
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-02-22 05:33:31 +0300

    add a prompt message for the day born

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,17 @@ window.addEventListener("DOMContentLoaded", function (){
 
     let user_date = document.getElementById("date").value;
   	let day = getDayName(user_date,"en-US");
-    // console.log("Your were born on:"+day);
+    prompt("Your were born on:"+day);
   
-    let gender = document.getElementById("gender").value;
+  //   let gender = document.querySelector("gender");
 
-    // let femaleNames = ["Akosua" "Adwoa" "Abenaa" "Akua" "Yua" "Afua" "Ama"];
-    // let maleNames = ["Kwasi" "Kwadwo" "Kwabena" "Kwaku" "Yaw" "Kofi" "Kwame"];
+  //   let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yua", "Afua", "Ama"];
+  //   let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     
-  function akanName(day, gender){
-      if (day === Monday && gender === male) console.log("Akosua");
-       else if (day === Monday && gender ==== female); console.log("Adwoa");
-    }
+  // function akanName(day, gender){
+  //     if (day = Monday && gender = male) console.log("Kwasi");
+  //      else if (day = Monday && gender = female) console.log("Adwoa");
+  // }
     
 
   
